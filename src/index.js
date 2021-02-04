5a3a203a35c7c03caa055b087ae398a897ab148f
Paula Rösler <paularoesler@web.de>
2021-02-04 15:58:48 +0100

    Add Button with JS

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import Header from './components/Header'
 const header = Header('Quiz App', 'May the best win!')
 document.body.append(header)
 
-const button = Button('submit')
+const button = Button('Click me')
+const button2 = Button('Hold me')
+
 document.body.append(button)
+document.body.append(button2)
