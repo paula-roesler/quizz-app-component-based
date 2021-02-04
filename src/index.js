9601fcf9163883908604f1af0c1120f04793868f
Paula Rösler <paularoesler@web.de>
2021-02-04 14:38:59 +0100

    Add Header to JS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import Button from './components/Button'
+import Header from './components/Header'
+
+const header = Header('Quiz App', 'May the best win!')
+document.body.append(header)
 
 const button = Button('submit')
 document.body.append(button)
