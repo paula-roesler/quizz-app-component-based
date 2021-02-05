43a4e17314c2058d36b121ed588de2a56a6e98e1
Paula Rösler <paularoesler@web.de>
2021-02-05 07:31:35 +0100

    Change createElement function to add children, update Components

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,5 @@ import createElement from '../lib/createElement'
 import './Button.css'
 
 export default function Button(text) {
-  const el = createElement('button', {
-    className: 'Button',
-    innerText: text,
-  })
-
-  return el
+  return createElement('button', { className: 'Button', textContent: text })
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import createElement from './lib/createElement'
 import Button from './components/Button'
+import Card from './components/Card'
 import Header from './components/Header'
 
-const header = Header('Quiz App', 'May the best win!')
-document.body.append(header)
-
-const button = Button('Click me')
-const button2 = Button('Hold me')
-
-document.body.append(button)
-document.body.append(button2)
+const grid = createElement(
+  'div',
+  { className: 'appGrid' },
+  Header('Quiz App', 'May the best win!'),
+  Card('Foo bar?', 'Bar baz!')
+  Button('Click me!'),
+  Button('Hold me!')
+)
