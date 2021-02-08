ce6a0ca6cae6c4790c9173133587edd411add9fb
Paula Rösler <paularoesler@web.de>
2021-02-08 15:26:25 +0100

    Add FontAwsome

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import createElement from './lib/createElement'
-import Button from './components/Button'
 import Card from './components/Card'
 import Header from './components/Header'
-import CreateForm from './components/CreateForm'
 
 const grid = createElement(
   'div',
   { className: 'appGrid' },
   Header('Quiz App', 'May the best win!'),
   Card('Foo bar?', 'Bar baz!')
-  Button('Click me!'),
-  Button('Hold me!')
 )
+
+document.body.append(grid)
